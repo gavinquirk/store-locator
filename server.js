cdac28e2debbcb3e0d8e29c5a532a580a31510cc
Gavin Quirk <gavinquirk@gmail.com>
2020-01-02 13:37:43 -0800

    controller and route file structure, db connection

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
+const connectDB = require('./config/db')
 
 // Load environment variables
 dotenv.config({
   path: './config/config.env'
 })
+
+// Connect to DB
+connectDB();
 
 const app = express();
 
@@ -17,9 +21,7 @@ app.use(express.json())
 app.use(cors())
 
 // Routes
-app.get('/api/v1/stores', (req, res) => {
-  res.send('Hello World')
-});
+app.use('/api/v1/stores', require('./routes/stores'))
 
 const PORT = process.env.PORT || 5000
 
