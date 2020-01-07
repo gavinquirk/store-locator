fec4a08b2230385c629f3d144a78c3a4a2cd825e
Gavin Quirk <gavinquirk@gmail.com>
2020-01-07 10:27:07 -0800

    mapbox html and js, map placeholder

---
+++ public/js/map.js
@@ -0,0 +1,46 @@
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiZ3F1aXJrIiwiYSI6ImNrNTQ2eWEzbTBnYm8zaGxxbXFjdTRyN2MifQ.OApzJqxHptQxDWPAMTSArQ';
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  zoom: 9,
+  center: [-71.157895, 42.707741]
+});
+
+function loadMap() {
+  map.on('load', function() {
+    map.addLayer({
+      id: 'points',
+      type: 'symbol',
+      source: {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: [
+            {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [-71.157895, 42.707741]
+              },
+              properties: {
+                storeId: '0001',
+                icon: 'shop'
+              }
+            }
+          ]
+        }
+      },
+      layout: {
+        'icon-image': '{icon}-15',
+        'icon-size': 1.5,
+        'text-field': '{storeId}',
+        'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+        'text-offset': [0, 0.9],
+        'text-anchor': 'top'
+      }
+    });
+  });
+}
+
+loadMap();
