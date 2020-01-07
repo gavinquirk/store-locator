234e1498c4429198740e3f0bbeaa4ea575b552e1
Gavin Quirk <gavinquirk@gmail.com>
2020-01-07 10:43:22 -0800

    retrieve stores from db and display

--- public/js/map.js
+++ public/js/map.js
@@ -7,7 +7,33 @@ const map = new mapboxgl.Map({
   center: [-71.157895, 42.707741]
 });
 
-function loadMap() {
+// Fetch stores from API
+async function getStores() {
+  const res = await fetch('/api/v1/stores');
+  const data = await res.json();
+
+  const stores = data.data.map(store => {
+    return {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          store.location.coordinates[0],
+          store.location.coordinates[1]
+        ]
+      },
+      properties: {
+        storeId: store.storeId,
+        icon: 'shop'
+      }
+    };
+  });
+
+  loadMap(stores);
+}
+
+// Load map with stores
+function loadMap(stores) {
   map.on('load', function() {
     map.addLayer({
       id: 'points',
@@ -16,19 +42,7 @@ function loadMap() {
         type: 'geojson',
         data: {
           type: 'FeatureCollection',
-          features: [
-            {
-              type: 'Feature',
-              geometry: {
-                type: 'Point',
-                coordinates: [-71.157895, 42.707741]
-              },
-              properties: {
-                storeId: '0001',
-                icon: 'shop'
-              }
-            }
-          ]
+          features: stores
         }
       },
       layout: {
@@ -43,4 +57,4 @@ function loadMap() {
   });
 }
 
-loadMap();
+getStores();
